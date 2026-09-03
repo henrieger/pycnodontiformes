fe8fce571ca1152acb0382537366fd93e4d0b892
Henrique Rieger <henriquerieger2001@gmail.com>
2026-09-03 15:18:07 -0300

    [add] new analysis of partitioned characters with revised data

--- 02_divergence/partitioned/join_alignment_tree.sh
+++ 02_divergence/partitioned/join_alignment_tree.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-cat ../../00_tree_inference/combined/costa_2026_anterior_portion.nex ../../01_mcctree/combined/rooted_mcc.nex |
+cat ../../00_tree_inference/partitioned/costa_2026_partitioned.nex ../../01_mcctree/partitioned/rooted_mcc.nex |
   dos2unix |
   tr '\n' '\000' |
   sed 's/#NEXUS//2' |
